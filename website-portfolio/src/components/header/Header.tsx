ab813949acec2c3b5af3868c89d68cc6af7c44b7
Oliver Riley <oliver.r2345@gmail.com>
2025-09-21 03:44:39 +0900

    header updates

--- website-portfolio/src/components/header/Header.tsx
+++ website-portfolio/src/components/header/Header.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 
 const Header: React.FC = () => (
-  <header className="w-full flex justify-between items-center px-6 py-4 bg-none">
-    <span className="text-lg font-bold text-gray-800">Left Text</span>
-    <span className="text-lg text-gray-600">Right Text</span>
+  <header className="w-full flex justify-between items-center px-6 py-4 static bg-opacity-0">
+    <span className="text-lg font-thin text-gray-800">Oliver's Portfolio</span>
   </header>
 );
 
