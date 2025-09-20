f7e2902849a0f44742d5cefd083b412f4c4815ba
Oliver Riley <oliver.r2345@gmail.com>
2025-09-21 03:19:29 +0900

    Added TailWindCSS compatability to the site.

---
+++ website-portfolio/src/pages/HomePage.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router";
+
+function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-100 to-blue-300">
+      <h1 className="text-4xl font-bold mb-4">Home Page</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default HomePage;
